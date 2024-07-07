1fbc4cfba938561c0feab6e4315064a2de60e404
Haritha-PV <harithapv21@gmail.com>
2024-07-07 13:32:13 +0530

    add files

--- Git/Self practice/environvar.py
+++ Git/Self practice/environvar.py
@@ -1,9 +1,18 @@
 import os
 
+#set
 os.environ['Setvar']="45ty67"
 
+#get
 env=os.getenv('Setvar')
 print(env, "\n")
 
 env2=os.environ.get('Setvar')
 print(env2)
+
+
+#output
+#/OneDrive/Desktop/Meta Scifor technologies/Haritha-P-V_Scifor/Haritha-P-V_Scifor/Git/Self practice/environvar.py"
+#45ty67
+
+#45ty67
